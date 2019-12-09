9595be16d2329065087c225cd7eebb7dfcf3e495
atf33 <57871932+atf33@users.noreply.github.com>
2019-12-08 19:37:08 -0500

    Added getFlightWithMostTicketsSold implementation

--- src/travelReservationSystem/AdminStatements.java
+++ src/travelReservationSystem/AdminStatements.java
@@ -45,17 +45,28 @@ public class AdminStatements {
 
 	}
 	static PreparedStatement getFlightWithMostTicketsSold(Connection con) throws SQLException {
+		String statement = (SELECT s.flightNumber
+				    FROM (SELECT t.flightNumber, COUNT(*) AS count
+					  FROM Ticket AS t
+					  GROUP BY t.flightNumber
+					  HAVING count = (SELECT COUNT(*) AS count
+							    FROM docs AS u
+							    GROUP BY u.flightNumber
+							    LIMIT 1)) AS s)
 
+		PreparedStatement ps = con.prepareStatement(statement);
+		
+		return ps;
 	}
-	static PreparedStatement getFlightsByAirport(Connection con, String airport) throws SQLException {
+	static PreparedStatement getFlightsByAirport(Connection con, String airportID) throws SQLException {
 		String statement = "SELECT * "
 				 + "FROM Flight " 
 				 + "WHERE DepartureAirport = ? AND ArrivalAirport = ?";
 		
 		PreparedStatement ps = con.prepareStatement(statement);
 		
-		ps.setString(1, airport);
-		ps.setString(2, airport);
+		ps.setString(1, airportID);
+		ps.setString(2, airportID);
 		return ps;		    
 	}
 	
